b5d6302790a36fb1cb6bb9bda1d96ace3a4d7d5d
Patrick Exner <patrick.exner1@web.de>
2024-04-21 12:39:37 +0200

    Refactor to always check which shape is currently active

--- editor/plugins/gizmos/gpu_particles_3d_emission_shape_gizmo_plugin.cpp
+++ editor/plugins/gizmos/gpu_particles_3d_emission_shape_gizmo_plugin.cpp
@@ -66,16 +66,30 @@ bool GPUParticles3DEmissionShapeGizmoPlugin::is_selectable_when_hidden() const {
 }
 
 String GPUParticles3DEmissionShapeGizmoPlugin::get_handle_name(const EditorNode3DGizmo *p_gizmo, int p_id, bool p_secondary) const {
-	if (p_id == 0) {
+	GPUParticles3D *particles = Object::cast_to<GPUParticles3D>(p_gizmo->get_node_3d());
+	Ref<ParticleProcessMaterial> mat = particles->get_process_material();
+	ParticleProcessMaterial::EmissionShape shape = mat->get_emission_shape();
+
+	if (shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE || shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE_SURFACE) {
 		return "Radius";
-	} else if (p_id == 1 || p_id == 2 || p_id == 3) {
-		return "Box Extends";
-	} else if (p_id == 4) {
-		return "Ring Height";
-	} else if (p_id == 5) {
-		return "Ring Radius";
-	} else if (p_id == 6) {
-		return "Ring Inner Radius";
+	} else if (shape == ParticleProcessMaterial::EMISSION_SHAPE_BOX) {
+		if (p_id == 0) {
+			return "Box Extents X";
+		} else if (p_id == 1) {
+			return "Box Extents Y";
+		} else {
+			return "Box Extents Z";
+		}
+	} else if (shape == ParticleProcessMaterial::EMISSION_SHAPE_RING) {
+		if (p_id == 0) {
+			return "Ring Axis";
+		} else if (p_id == 1) {
+			return "Ring Height";
+		} else if (p_id == 2) {
+			return "Ring Radius";
+		} else {
+			return "Ring Inner Radius";
+		}
 	} else {
 		return "";
 	}
@@ -84,17 +98,28 @@ String GPUParticles3DEmissionShapeGizmoPlugin::get_handle_name(const EditorNode3
 Variant GPUParticles3DEmissionShapeGizmoPlugin::get_handle_value(const EditorNode3DGizmo *p_gizmo, int p_id, bool p_secondary) const {
 	GPUParticles3D *particles = Object::cast_to<GPUParticles3D>(p_gizmo->get_node_3d());
 	Ref<ParticleProcessMaterial> mat = particles->get_process_material();
+	ParticleProcessMaterial::EmissionShape shape = mat->get_emission_shape();
 
-	if (p_id == 0) {
+	if (shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE || shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE_SURFACE) {
 		return mat->get_emission_sphere_radius();
-	} else if (p_id == 1 || p_id == 2 || p_id == 3) {
-		return mat->get_emission_box_extents();
-	} else if (p_id == 4) {
-		return mat->get_emission_ring_height();
-	} else if (p_id == 5) {
-		return mat->get_emission_ring_radius();
-	} else if (p_id == 6) {
-		return mat->get_emission_ring_inner_radius();
+	} else if (shape == ParticleProcessMaterial::EMISSION_SHAPE_BOX) {
+		if (p_id == 0) {
+			return mat->get_emission_box_extents().x;
+		} else if (p_id == 1) {
+			return mat->get_emission_box_extents().y;
+		} else {
+			return mat->get_emission_box_extents().z;
+		}
+	} else if (shape == ParticleProcessMaterial::EMISSION_SHAPE_RING) {
+		if (p_id == 0) {
+			return mat->get_emission_ring_axis();
+		} else if (p_id == 1) {
+			return mat->get_emission_ring_height();
+		} else if (p_id == 2) {
+			return mat->get_emission_ring_radius();
+		} else {
+			return mat->get_emission_ring_inner_radius();
+		}
 	} else {
 		return Variant();
 	}
@@ -103,85 +128,109 @@ Variant GPUParticles3DEmissionShapeGizmoPlugin::get_handle_value(const EditorNod
 void GPUParticles3DEmissionShapeGizmoPlugin::set_handle(const EditorNode3DGizmo *p_gizmo, int p_id, bool p_secondary, Camera3D *p_camera, const Point2 &p_point) {
 	GPUParticles3D *particles = Object::cast_to<GPUParticles3D>(p_gizmo->get_node_3d());
 	Ref<ParticleProcessMaterial> mat = particles->get_process_material();
+	ParticleProcessMaterial::EmissionShape shape = mat->get_emission_shape();
+
 	Transform3D gt = particles->get_global_transform();
 	Transform3D gi = gt.affine_inverse();
-
 	Vector3 ray_from = p_camera->project_ray_origin(p_point);
 	Vector3 ray_dir = p_camera->project_ray_normal(p_point);
-
 	Vector3 s[2] = { gi.xform(ray_from), gi.xform(ray_from + ray_dir * 4096) };
 	Vector3 ra, rb;
 	float d;
 
-	switch (p_id) {
-		case 0:
-			Geometry3D::get_closest_points_between_segments(Vector3(), Vector3(4096, 0, 0), s[0], s[1], ra, rb);
-			d = ra.x;
-			if (Node3DEditor::get_singleton()->is_snap_enabled()) {
-				d = Math::snapped(d, Node3DEditor::get_singleton()->get_translate_snap());
-			}
-			if (d < 0.001) { // Equal is here for negative zero.
-				d = 0.001;
-			}
-
-			mat->set_emission_sphere_radius(d);
-		case 1:
-			// TODO: Fix geometry handling
+	if (shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE || shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE_SURFACE) {
+		Geometry3D::get_closest_points_between_segments(Vector3(), Vector3(4096, 0, 0), s[0], s[1], ra, rb);
+		d = ra.x;
+		if (Node3DEditor::get_singleton()->is_snap_enabled()) {
+			d = Math::snapped(d, Node3DEditor::get_singleton()->get_translate_snap());
+		}
+		if (d < 0.001) { // Equal is here for negative zero.
+			d = 0.001;
+		}
+		mat->set_emission_sphere_radius(d);
+	} else if (shape == ParticleProcessMaterial::EMISSION_SHAPE_BOX) {
+		if (p_id == 0) {
 			Geometry3D::get_closest_points_between_segments(Vector3(-4096, 0, 0), Vector3(4096, 0, 0), s[0], s[1], ra, rb);
 			d = ra.x;
 			if (Node3DEditor::get_singleton()->is_snap_enabled()) {
 				d = Math::snapped(d, Node3DEditor::get_singleton()->get_translate_snap());
 			}
-
 			if (d <= 0.001) { // Equal is here for negative zero.
 				d = 0.001;
 			}
-
 			mat->set_emission_box_extents(Vector3(d, mat->get_emission_box_extents().y, mat->get_emission_box_extents().z));
-		case 2:
-			// TODO: Fix geometry handling
+		} else if (p_id == 1) {
 			Geometry3D::get_closest_points_between_segments(Vector3(0, -4096, 0), Vector3(0, 4096, 0), s[0], s[1], ra, rb);
-
 			d = ra.y;
 			if (Node3DEditor::get_singleton()->is_snap_enabled()) {
 				d = Math::snapped(d, Node3DEditor::get_singleton()->get_translate_snap());
 			}
-
 			if (d <= 0.001) { // Equal is here for negative zero.
 				d = 0.001;
 			}
-
 			mat->set_emission_box_extents(Vector3(mat->get_emission_box_extents().x, d, mat->get_emission_box_extents().z));
-		case 3:
+		} else {
 			Geometry3D::get_closest_points_between_segments(Vector3(0, 0, -4096), Vector3(0, 0, 4096), s[0], s[1], ra, rb);
-
 			d = ra.z;
 			if (Node3DEditor::get_singleton()->is_snap_enabled()) {
 				d = Math::snapped(d, Node3DEditor::get_singleton()->get_translate_snap());
 			}
-
 			if (d <= 0.001) { // Equal is here for negative zero.
 				d = 0.001;
 			}
-
 			mat->set_emission_box_extents(Vector3(mat->get_emission_box_extents().x, mat->get_emission_box_extents().y, d));
-		case 4:
-			Geometry3D::get_closest_points_between_segments(Vector3(0, 0, -4096), Vector3(0, 0, 4096), s[0], s[1], ra, rb);
-			// TODO: Ring height handle
-		case 5:
-			// TODO: Ring radius handle
-		case 6:
-			// TODO: Ring inner radius handle
+		}
+	} else if (shape == ParticleProcessMaterial::EMISSION_SHAPE_RING) {
+		WARN_PRINT_ONCE("Ring emission shape active");
+		Vector3 ring_axis = mat->get_emission_ring_axis();
+		Basis axis_basis = Basis();
+		axis_basis.rows[1] = ring_axis.normalized();
+		axis_basis.rows[0] = (Vector3(axis_basis[1][1], -axis_basis[1][2], -axis_basis[1][0])).normalized();
+		axis_basis.rows[0] = (axis_basis[0] - axis_basis[0].dot(axis_basis[1]) * axis_basis[1]).normalized();
+		axis_basis[2] = (axis_basis[0].cross(axis_basis[1])).normalized();
+		//axis_basis = axis_basis.inverse();
+
+		if (p_id == 1) {
+			Geometry3D::get_closest_points_between_segments(axis_basis.xform(Vector3(0, -4096, 0)), axis_basis.xform(Vector3(0, 4096, 0)), s[0], s[1], ra, rb);
+			d = ra.x;
+			if (Node3DEditor::get_singleton()->is_snap_enabled()) {
+				d = Math::snapped(d, Node3DEditor::get_singleton()->get_translate_snap());
+			}
+			if (d <= 0.001) { // Equal is here for negative zero.
+				d = 0.001;
+			}
+			mat->set_emission_ring_height(d);
+		} else if (p_id == 2) {
+			Geometry3D::get_closest_points_between_segments(axis_basis.xform(Vector3(0, 0, -4096)), axis_basis.xform(Vector3(0, 0, 4096)), s[0], s[1], ra, rb);
+			d = ra.y;
+			if (Node3DEditor::get_singleton()->is_snap_enabled()) {
+				d = Math::snapped(d, Node3DEditor::get_singleton()->get_translate_snap());
+			}
+			if (d <= 0.001) { // Equal is here for negative zero.
+				d = 0.001;
+			}
+			mat->set_emission_ring_radius(d);
+		} else {
+			Geometry3D::get_closest_points_between_segments(Vector3(-4096, 0, 0), Vector3(4096, 0, 0), s[0], s[1], ra, rb);
+			d = ra.z;
+			if (Node3DEditor::get_singleton()->is_snap_enabled()) {
+				d = Math::snapped(d, Node3DEditor::get_singleton()->get_translate_snap());
+			}
+			if (d <= 0.001) { // Equal is here for negative zero.
+				d = 0.001;
+			}
+			mat->set_emission_ring_inner_radius(d);
+		}
 	}
 }
 
 void GPUParticles3DEmissionShapeGizmoPlugin::commit_handle(const EditorNode3DGizmo *p_gizmo, int p_id, bool p_secondary, const Variant &p_restore, bool p_cancel) {
 	GPUParticles3D *particles = Object::cast_to<GPUParticles3D>(p_gizmo->get_node_3d());
 	Ref<ParticleProcessMaterial> mat = particles->get_process_material();
-
 	ParticleProcessMaterial *process_mat = Object::cast_to<ParticleProcessMaterial>(mat.ptr());
+	ParticleProcessMaterial::EmissionShape shape = mat->get_emission_shape();
 
-	if (p_id == 0) {
+	if (shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE || shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE_SURFACE) {
 		if (p_cancel) {
 			mat->set_emission_sphere_radius(p_restore);
 		} else {
@@ -191,45 +240,83 @@ void GPUParticles3DEmissionShapeGizmoPlugin::commit_handle(const EditorNode3DGiz
 			ur->add_undo_property(process_mat, "emission_sphere_radius", p_restore);
 			ur->commit_action();
 		}
-	} else if (p_id == 1 || p_id == 2 || p_id == 3) {
-		if (p_cancel) {
-			mat->set_emission_box_extents(p_restore);
+	} else if (shape == ParticleProcessMaterial::EMISSION_SHAPE_BOX) {
+		Vector3 size = mat->get_emission_box_extents();
+		if (p_id == 0) {
+			if (p_cancel) {
+				// TODO: Handle commit handle Box X and display the handle
+				mat->set_emission_box_extents(Vector3(p_restore, size.y, size.z));
+			} else {
+				EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
+				ur->create_action(TTR("Change emission box extent X"));
+				ur->add_do_property(process_mat, "emission_box_extents", process_mat->get_emission_box_extents());
+				ur->add_undo_property(process_mat, "emission_box_extents", Vector3(p_restore, size.y, size.z));
+				ur->commit_action();
+			}
+		} else if (p_id == 1) {
+			if (p_cancel) {
+				// TODO: Handle commit handle Box Y and display the handle
+				mat->set_emission_box_extents(Vector3(p_restore));
+			} else {
+				EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
+				ur->create_action(TTR("Change emission box extent Y"));
+				ur->add_do_property(process_mat, "emission_box_extents", process_mat->get_emission_box_extents());
+				ur->add_undo_property(process_mat, "emission_box_extents", Vector3(size.x, p_restore, size.z));
+				ur->commit_action();
+			}
 		} else {
-			EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
-			ur->create_action(TTR("Change emission box extents"));
-			ur->add_do_property(process_mat, "emission_box_extents", process_mat->get_emission_box_extents());
-			ur->add_undo_property(process_mat, "emission_box_extents", p_restore);
-			ur->commit_action();
+			if (p_cancel) {
+				// TODO: Handle commit handle Box Z and display the handle
+				mat->set_emission_box_extents(Vector3(p_restore));
+			} else {
+				EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
+				ur->create_action(TTR("Change emission box extent Z"));
+				ur->add_do_property(process_mat, "emission_box_extents", process_mat->get_emission_box_extents());
+				ur->add_undo_property(process_mat, "emission_box_extents", Vector3(size.x, size.y, p_restore));
+				ur->commit_action();
+			}
 		}
-	} else if (p_id == 4) {
-		if (p_cancel) {
-			mat->set_emission_ring_height(p_restore);
+	} else if (shape == ParticleProcessMaterial::EMISSION_SHAPE_RING) {
+		if (p_id == 0) {
+			if (p_cancel) {
+				mat->set_emission_ring_axis(p_restore);
+			} else {
+				EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
+				ur->create_action(TTR("Change emission ring axis"));
+				ur->add_do_property(process_mat, "emission_ring_axis", process_mat->get_emission_ring_axis());
+				ur->add_undo_property(process_mat, "emission_ring_axis", p_restore);
+				ur->commit_action();
+			}
+		} else if (p_id == 1) {
+			if (p_cancel) {
+				mat->set_emission_ring_height(p_restore);
+			} else {
+				EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
+				ur->create_action(TTR("Change emission ring height"));
+				ur->add_do_property(process_mat, "emission_ring_height", process_mat->get_emission_ring_height());
+				ur->add_undo_property(process_mat, "emission_ring_height", p_restore);
+				ur->commit_action();
+			}
+		} else if (p_id == 2) {
+			if (p_cancel) {
+				mat->set_emission_ring_radius(p_restore);
+			} else {
+				EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
+				ur->create_action(TTR("Change emission ring radius"));
+				ur->add_do_property(process_mat, "emission_ring_radius", process_mat->get_emission_ring_radius());
+				ur->add_undo_property(process_mat, "emission_ring_radius", p_restore);
+				ur->commit_action();
+			}
 		} else {
-			EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
-			ur->create_action(TTR("Change emission ring height"));
-			ur->add_do_property(process_mat, "emission_ring_height", process_mat->get_emission_ring_height());
-			ur->add_undo_property(process_mat, "emission_ring_height", p_restore);
-			ur->commit_action();
-		}
-	} else if (p_id == 5) {
-		if (p_cancel) {
-			mat->set_emission_ring_radius(p_restore);
-		} else {
-			EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
-			ur->create_action(TTR("Change emission ring radius"));
-			ur->add_do_property(process_mat, "emission_ring_radius", process_mat->get_emission_ring_radius());
-			ur->add_undo_property(process_mat, "emission_ring_radius", p_restore);
-			ur->commit_action();
-		}
-	} else if (p_id == 6) {
-		if (p_cancel) {
-			mat->set_emission_ring_inner_radius(p_restore);
-		} else {
-			EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
-			ur->create_action(TTR("Change emission ring inner radius"));
-			ur->add_do_property(process_mat, "emission_ring_inner_radius", process_mat->get_emission_ring_inner_radius());
-			ur->add_undo_property(process_mat, "emission_ring_inner_radius", p_restore);
-			ur->commit_action();
+			if (p_cancel) {
+				mat->set_emission_ring_inner_radius(p_restore);
+			} else {
+				EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
+				ur->create_action(TTR("Change emission ring inner radius"));
+				ur->add_do_property(process_mat, "emission_ring_inner_radius", process_mat->get_emission_ring_inner_radius());
+				ur->add_undo_property(process_mat, "emission_ring_inner_radius", p_restore);
+				ur->commit_action();
+			}
 		}
 	}
 }
@@ -243,8 +330,7 @@ void GPUParticles3DEmissionShapeGizmoPlugin::redraw(EditorNode3DGizmo *p_gizmo) 
 		Ref<ParticleProcessMaterial> mat = particles->get_process_material();
 		ParticleProcessMaterial::EmissionShape shape = mat->get_emission_shape();
 
-		const Ref<Material> material =
-				get_material("particles_emission_shape_material", p_gizmo);
+		const Ref<Material> material = get_material("particles_emission_shape_material", p_gizmo);
 		Ref<Material> handles_material = get_material("handles");
 
 		if (shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE || shape == ParticleProcessMaterial::EMISSION_SHAPE_SPHERE_SURFACE) {
@@ -286,8 +372,9 @@ void GPUParticles3DEmissionShapeGizmoPlugin::redraw(EditorNode3DGizmo *p_gizmo) 
 			for (int i = 0; i < 12; i++) {
 				Vector3 a, b;
 				box_aabb.get_edge(i, a, b);
-				lines.push_back(a * scale * box_extents + offset);
-				lines.push_back(b * scale * box_extents + offset);
+				// Multiplication by 2 due to the extents being only half of the box size
+				lines.push_back(a * 2.0 * scale * box_extents + offset);
+				lines.push_back(b * 2.0 * scale * box_extents + offset);
 			}
 
 			Vector<Vector3> handles;
